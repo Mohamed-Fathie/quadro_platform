32b6a7f2b42e6bdb72cd67dec706740742d6eeca
عبدالمؤمن حسين <An.Amir@uot.edu.ly>
2024-12-07 14:21:26 +0200

    towing screens & NavBar added
    towing screens & NavBar added

---
+++ quadro_platform/lib/constants/utils/textStyles.dart
@@ -0,0 +1,213 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:quadro_platform/constants/utils/colors.dart';
+import 'package:sizer/sizer.dart';
+
+
+class AppTextStyles {
+  static TextStyle authButtonTextStyle = TextStyle(
+    fontSize: 11.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+  );
+  static TextStyle textFieldTextStyle = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+  static TextStyle textFieldHintTextStyle = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: grey,
+  );
+// ! *************** UBER MOVE MEDIUM *******************
+  static TextStyle heading26 = TextStyle(
+    fontSize: 26.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+  static TextStyle heading24 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+  static TextStyle heading22 = TextStyle(
+    fontSize: 22.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+  static TextStyle heading20 = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+  static TextStyle body18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+  static TextStyle body16 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+  static TextStyle body14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+
+  static TextStyle small12 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+  static TextStyle small10 = TextStyle(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+  static TextStyle small8 = TextStyle(
+    fontSize: 8.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveMedium',
+    color: black,
+  );
+  // ! *************** UBER MOVE BOLD *******************
+  static TextStyle heading26Bold = TextStyle(
+    fontSize: 26.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+    color: black,
+  );
+  static TextStyle heading24Bold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+    color: black,
+  );
+  static TextStyle heading22Bold = TextStyle(
+    fontSize: 22.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+    color: black,
+  );
+  static TextStyle heading20Bold = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+    color: black,
+  );
+  static TextStyle body18Bold = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+    color: black,
+  );
+  static TextStyle body16Bold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+    color: black,
+  );
+  static TextStyle body14Bold = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+    color: black,
+  );
+
+  static TextStyle small12Bold = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+    color: black,
+  );
+  static TextStyle small10Bold = TextStyle(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+    color: black,
+  );
+  static TextStyle small8Bold = TextStyle(
+    fontSize: 8.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'UberMoveBold',
+    color: black,
+  );
+
+ // ************** MADHANI **************
+  static TextStyle Mheading26Bold = TextStyle(
+    fontSize: 26.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Madhani-Arabic',
+    color: black,
+  );
+  static TextStyle Mheading24Bold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Madhani-Arabic',
+    color: black,
+  );
+  static TextStyle Mheading22Bold = TextStyle(
+    fontSize: 22.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Madhani-Arabic',
+    color: black,
+  );
+  static TextStyle Mheading20Bold = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Madhani-Arabic',
+    color: black,
+  );
+  static TextStyle Mbody18Bold = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Madhani-Arabic',
+    color: black,
+  );
+  static TextStyle Mbody16Bold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Madhani-Arabic',
+    color: black,
+  );
+  static TextStyle Mbody14Bold = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Madhani-Arabic',
+    color: black,
+  );
+
+  static TextStyle Msmall12Bold = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Madhani-Arabic',
+    color: black,
+  );
+  static TextStyle Msmall10Bold = TextStyle(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Madhani-Arabic',
+    color: black,
+  );
+  static TextStyle Msmall8Bold = TextStyle(
+    fontSize: 8.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Madhani-Arabic',
+    color: black,
+  );
+}
--- quadro_platform/lib/main.dart
+++ quadro_platform/lib/main.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quadro_platform/user/controller/BottomNavBarProvider/bottom_nav_bar_provider.dart';
+import 'package:quadro_platform/user/view/account/user_account_screen.dart';
+import 'package:quadro_platform/user/view/bottomNavBar/bottom_navbar.dart';
+import 'package:quadro_platform/user/view/riderActivityScreen/rider_activity_screen.dart';
+import 'package:quadro_platform/user/view/riderHomeScreen/rider_home_screen.dart';
 import 'package:quadro_platform/theme/globalthemdata.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Quadro());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Quadro extends StatelessWidget {
+  const Quadro({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: GlobalThemData.lightThemeData,
-      darkTheme: GlobalThemData.darkThemeData,
-      themeMode: ThemeMode.system,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("qadro app"),
-        ),
-        body: const Center(
-          child: Text("hi!!"),
-        ),
-      ),
+    return Sizer(
+      builder: (context, _, __) {
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider<BottomNavBarProvider>(
+              create: (_) => BottomNavBarProvider(),
+            ),
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: GlobalThemData.lightThemeData,
+            darkTheme: GlobalThemData.darkThemeData,
+            themeMode: ThemeMode.system,
+            home: BottomNavBar(),
+          ),
+        );
+      },
     );
   }
 }
