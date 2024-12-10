18add505323430ac6cab7c790fa97b81cb767203
mohamed fathie <M.jdoor@uot.edu.ly>
2024-12-10 11:47:22 +0200

    firebase setup done

--- quadro_platform/lib/main.dart
+++ quadro_platform/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:quadro_platform/features/onboarding/view/on_boarding_page.dart';
 import 'package:quadro_platform/features/theme/globalthemdata.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
